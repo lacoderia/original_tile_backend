1dd2e78e71039c07e6904e14753324a207013f04
tonklis <benjamin.hernandez@in2teck.com>
2016-07-11 17:21:18 -0500

    Removed byebug breakpoint.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,8 +9,6 @@ feature 'UsersController' do
       login_with_service user = { email: user_01.email, password: "12345678" }
       access_token_1, uid_1, client_1, expiry_1, token_type_1 = get_headers
       set_headers access_token_1, uid_1, client_1, expiry_1, token_type_1
-
-      byebug
       
     end
 
