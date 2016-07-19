fbd84510b7e28434eb370b506bb03f72ffc1e3eb
tonklis <benjamin.hernandez@in2teck.com>
2016-07-19 03:16:24 -0500

    Added seeds for colors, tiles and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,11 @@
 internal_user_role = Role.create(name: 'internal_user')
 external_user_role = Role.create(name: 'external_user')
 
-internal_user = User.create(first_name: "Internal", last_name: "Quimare", email: "internal@email.com", roles: [internal_user_role], password: 'password', password_confirmation: 'password')
-external_user = User.create(first_name: "External", last_name: "Quimare", email: "external@email.com", roles: [external_user_role], password: 'password', password_confirmation: 'password')
+internal_user = User.create(first_name: "Internal", last_name: "Quimare", email: "internal@email.com", roles: [internal_user_role], password: 'password', password_confirmation: 'password', active: true)
+external_user = User.create(first_name: "External", last_name: "Quimare", email: "external@email.com", roles: [external_user_role], password: 'password', password_confirmation: 'password', active: true)
 
 admin_user = AdminUser.create!(email: 'admin@originalmissiontile.com', password: 'password', password_confirmation: 'password')
+
+color = Color.create!(name: "red", hex_value: "#FF0000", active: true)
+tile = Tile.create!(url: "file_url", tile_type: "3x3", active: true)
+project = Project.create!(url: "file_url", name: "saved project 1", user_id: internal_user.id)
