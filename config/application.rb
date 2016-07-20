380da9f9f4918bc9e4ce8bfba0f4e1c3fb4bf220
tonklis <benjamin.hernandez@in2teck.com>
2016-07-20 16:54:25 -0500

    Added put for access-resource-origin

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module OriginalTile
         resource '*', 
           :headers => :any, 
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :methods => [:get, :post, :options]
+          :methods => [:get, :post, :options, :put]
       end
     end
 
