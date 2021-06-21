00889ea6fdeb9f1fc01f2c16d7ce62b00032123f
tonklis <benjamin.hernandez@in2teck.com>
2021-06-21 14:56:43 -0500

    Made project serializer more efficient avoiding xml and image

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,4 +1,24 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :url, :name, :filename, :created_at, :tiles, :description
-  has_many :tiles
+  attributes :id, :url, :name, :filename, :created_at, :tiles, :description, :tiles
+
+  def tiles
+    tile_array = []
+    if not object.tiles.empty?
+      tile_array = object.tiles.map do |tile|
+        {
+          id: tile.id,
+          name: tile.name,
+          sku: tile.sku,
+          tile_type: {
+            key: tile.tile_type.key,
+            value: tile.tile_type.value,
+            centimeters: tile.tile_type.centimeters,
+            inches: tile.tile_type.inches,
+            shape: tile.tile_type.shape
+          }
+        }
+      end
+    end
+    tile_array
+  end
 end
